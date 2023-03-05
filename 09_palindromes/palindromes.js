1d2e413116838c45aaafe079abd05e4828e102a3
Jazz <JazzDevours@gmail.com>
2023-03-05 12:44:19 -0500

    complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+  let str = string.toLowerCase();
+  let arr = str.split("");
+  let pal = [];
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] == arr[i].match(/[a-z]/)) {
+        arr1.push(arr[i]);
+      }
+    }
+  for (let i = arr1.length - 1; i >= 0; i--) {
+    pal.push(arr1[i]);
+  }
+  return (arr1.toString() == pal.toString());
+}
 
 // Do not edit below this line
 module.exports = palindromes;
