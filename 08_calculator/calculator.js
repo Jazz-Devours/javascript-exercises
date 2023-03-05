8b5bb889b04b50dff68bef19b8439a4e0ae1d247
Jazz <JazzDevours@gmail.com>
2023-03-05 11:14:59 -0500

    exercise 08_calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total;
+	for (let i = 0; i < arr.length; i++) {
+    if (i == 0) {
+      total = arr[i];
+    }
+    else {
+      total *= arr[i];
+    }
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total;
+  for (let i = 0; i < b; i++) {
+    if (i == 0) {
+      total = a;
+    }
+    else {
+      total *= a;
+    }
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	let total = 1;
+  if (val < 0) return undefined;
+  if (val < 2) return total;
+  else {
+    for (let i = val; i > 1; i--) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
