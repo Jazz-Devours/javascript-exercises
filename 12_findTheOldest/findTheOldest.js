e1d1951fd8fb3443a03259d2d7ce023821f7b261
Jazz <JazzDevours@gmail.com>
2023-03-05 14:31:11 -0500

    complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const currentDate = new Date().getFullYear();
+  const oldest = array.sort(function (a, b) {
+    let lastGuy;
+    let nextGuy;
+    typeof a.yearOfDeath == "number" ? lastGuy = (a.yearOfDeath - a.yearOfBirth) : lastGuy = (currentDate - a.yearOfBirth);
+    typeof b.yearOfDeath == "number" ? nextGuy = (b.yearOfDeath - b.yearOfBirth) : nextGuy = (currentDate - b.yearOfBirth);
+    return lastGuy > nextGuy ? -1 : 1;
+  })
+  return oldest[0];
 };
 
 // Do not edit below this line
