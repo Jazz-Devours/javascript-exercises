1092f9bf02e42f5a39567935929762aaab8d2a1a
Jazz <JazzDevours@gmail.com>
2023-03-05 13:12:26 -0500

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let fib = [0, 1];
+  if (n < 0) {
+    return "OOPS";
+  }
+  for (let i = 0; i < n-1; i++) {
+    fib.push(fib[i]+fib[i+1]);
+  }
+  return fib[n];
 };
 
 // Do not edit below this line
